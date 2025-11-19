6daaed627d709bb673732d880121c44009ad34d3
Barbara Skladanowska <bskladanowska@gmail.com>
2025-11-19 13:57:29 +0100

    Corrected error handling and some minor errors.

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app.secret_key = 'your_secret_key'
 def register():
     data = request.json
     user_data_needed = ['first_name', 'last_name', 'email', 'phone_number', 'user_name', 'password', 'user_type_id']
-    
+
     #validating user input
     if not all(key in data for key in user_data_needed):
         return jsonify({'error': 'Missing user data'}), 400
-    elif data['first_name'].isnumeric() or data['last_name'].isnumeric():
+    elif not data['first_name'].isalpha() or not data['last_name'].isalpha():
         return jsonify({'error': 'First name and last name must contain only letters'}), 400
     elif not data['phone_number'].isnumeric() or len(data['phone_number']) != 9:
         return jsonify({'error': 'Phone number must be numeric and exactly 9 digits long'}), 400
@@ -20,8 +20,8 @@ def register():
         return jsonify({'error': 'Invalid email format'}), 400
     elif not data['user_name'].isalnum() or len(data['user_name']) < 5:
         return jsonify({'error': 'Username must be alphanumeric and at least 5 characters long'}), 400
-    elif db.get_user_by_phone_number(data['phone_number']) and db.get_user_by_email(data['email']):
-        return jsonify({'error': 'Phone number and email already registered'}), 400
+    #elif db.get_user_by_phone_number(data['phone_number']) and db.get_user_by_email(data['email']):
+        #return jsonify({'error': 'Phone number and email already registered'}), 400
     elif db.get_user_by_username(data['user_name']):
         return jsonify({'error': 'Username already exists'}), 400
     elif len(data['password']) < 10:
@@ -36,16 +36,21 @@ def register():
         return jsonify({'error': 'Password must contain at least one special character'}), 400
     elif ' ' in data['password']:
         return jsonify({'error': 'Password must not contain spaces'}), 400   
-    
+    else: 
     #adding user to the database
-    user_id = db.add_user(
-        data['first_name'],
-        data['last_name'],
-        data['email'],
-        data['phone_number'],
-        data['user_name'],
-        data['password'],
-        data['user_type_id']
-    )
-    return jsonify({'message': 'User registered successfully', 'user_id': user_id}), 201
+        user_id = db.add_user(
+            data['first_name'],
+            data['last_name'],
+            data['email'],
+            data['phone_number'],
+            data['user_name'],
+            data['password'],
+            data['user_type_id']
+        )
+        if user_id:
+            return jsonify({'message': 'User registered successfully', 'user_id': user_id}), 201
+        else:
+            return jsonify({'error': 'Registration failed'}), 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
