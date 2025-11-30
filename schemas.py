4021fc967e2e2c4cce4c81182c605e6cee0dfb6b
Barbara Skladanowska <bskladanowska@gmail.com>
2025-11-30 14:08:17 +0100

    Dodanie schematuów walidacji danych użytkownika

---
+++ schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field, EmailStr, ValidationError, field_validator
+import string
+import re
+
+class UserRegistrationModel(BaseModel):
+    first_name: str=Field(..., pattern=r'^[A-Za-z]+$')
+    last_name: str=Field(..., pattern=r'^[A-Za-z]+$')
+    email: EmailStr=Field(...)
+    phone_number: str=Field(..., pattern=r'^\d{9}$')
+    user_name: str=Field(..., min_length=5)
+    password: str=Field(..., min_length=10)
+    user_type_id: int=Field(...)
+
+    @field_validator('password')
+    def validate_password(cls, value: str) -> str:
+        if ' ' in value:
+            raise ValueError('Password must not contain spaces')
+        if not any(char.isdigit() for char in value):
+            raise ValueError('Password must contain at least one digit')
+        if not any(char.isupper() for char in value):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not any(char.islower() for char in value):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not any(char in string.punctuation for char in value):
+            raise ValueError('Password must contain at least one special character')
+        return value
+    
+    @field_validator('user_name')
+    def validate_user_name(cls, value: str) -> str:
+        pattern=r'^(?=.*\d)(?=.*[a-zA-Z])[A-Za-z0-9]+$'
+        if not re.match(pattern, value):
+            raise ValueError('Username must not contain spaces')
+        return value
