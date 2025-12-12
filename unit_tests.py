b021ea2249251418b8b7a60002a8cd712b01f4cc
Barbara Skladanowska <bskladanowska@gmail.com>
2025-12-12 22:22:35 +0100

    Added unit tests and minor correction to app

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,12 @@
 import unittest
 from datetime import datetime, timedelta
 from classes import User, Reservation
+from pydantic import ValidationError
+from schemas import UserRegistrationModel, ReservationModel
 
 class Unit_testing(unittest.TestCase):
-    #User class methods test
+
+    '------------------------USER CLASS TESTS---------------------------------'
     def test_user_is_staff_proprety(self):
         waiter = User(id=1, first_name='Anna', last_name='Kowalska', email='akowalska@test.pl', phone_number='123456789', user_type_id=3)
         self.assertTrue(waiter.is_staff)
@@ -18,14 +21,18 @@ class Unit_testing(unittest.TestCase):
         self.assertFalse(guest.is_staff)
     
     def test_user_is_manager_property(self):
-        waiter = User(id=1, first_name='Anna', last_name='Kowalska', email='akowalska@test.pl', phone_number='123456789', user_type_id=3)
-        self.assertTrue(waiter.is_staff)
-
         manager = User(id=3, first_name='Hanna', last_name='Kowalska', email='hkowalska@test.pl', phone_number='123456789', user_type_id=4)
         self.assertTrue(manager.is_manager)
 
-        guest = User(id=4, first_name='Max', last_name='Kowalski', email='mkowalska@test.pl', phone_number='123456789', user_type_id=1)
-        self.assertFalse(guest.is_staff)
+        owner = User(id=4, first_name='Max', last_name='Kowalski', email='mkowalska@test.pl', phone_number='123456789', user_type_id=5)
+        self.assertTrue(owner.is_manager)
+        self.assertTrue(owner.is_staff)
 
         member = User(id=2, first_name='Jan', last_name='Kowalski', email='jkowalska@test.pl', phone_number='123456789', user_type_id=2)
-        self.assertFalse(member.is_staff)
+        self.assertFalse(member.is_manager)
+
+        waiter = User(id=1, first_name='Anna', last_name='Kowalska', email='akowalska@test.pl', phone_number='123456789', user_type_id=3)
+        self.assertFalse(waiter.is_manager)
+    
+    '----------------------------RESERVATION CLASS TESTS------------------------------'
+    
