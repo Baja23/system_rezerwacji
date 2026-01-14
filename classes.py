56dc21f486cd605546e936f1b756aa6a56358564
BSkladanowska <99838805+Baja23@users.noreply.github.com>
2026-01-14 13:00:54 +0100

    Backend (#12)
    * Added db functions and most class functions
    
    * Added listing reservations functions to app and db
    
    * gitignore changes
    
    * Modified gitignore
    
    * Added some functionality to db, classes and app
    
    * Added unit tests and minor correction to app
    
    * Added unittests, corrected errors in schemas
    
    * Added files to gitignore
    
    * Adjusted files to frontend and added todo stuff
    
    * Corrected errors in backend
    
    * Corrected validate_time
    
    * Corrected validate_end_time
    
    * Modified reservation creation.
    
    * Corrected user_id issues
    
    * Corrected end time validation.
    
    * Corrected datetime issues
    
    * Corrected all validation issues
    
    * Corrected more datetime errors
    
    * Continuing the battle against datetime
    
    * Corrected some pydantic logic
    
    * Readded backend files
    
    * Added listing reservations functions to app and db
    
    * gitignore changes
    
    * Added merged gitignore
    
    * Added some functionality to db, classes and app
    
    * Added unit tests and minor correction to app
    
    * Resolved conflict in schemas.py
    
    * Added files to gitignore
    
    * Resolved conflict in schemas.py
    
    * Corrected errors in backend
    
    * Corrected validate_time
    
    * Modified reservation creation.
    
    * Corrected user_id issues
    
    * Resolved conflict in schemas.py
    
    * Resolved conflict in schemas.py
    
    * Resolved conflict in schemas.py
    
    * Corrected session issues and changed data type of
    available tables

---
+++ classes.py
@@ -0,0 +1,136 @@
+import database as db
+from werkzeug.security import check_password_hash
+
+class User:
+    def __init__(self, first_name, last_name, email, phone_number, user_type_id, user_name = None, password = None, id =  None):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.user_type_id = user_type_id
+        self.user_name = user_name
+        self.password = password
+
+    def save_user_info(self):
+        unique_columns = {'email': self.email, 'userName': self.user_name}
+        for key in unique_columns.keys():
+            user_in_db = db.get_user(key, unique_columns[key])
+        if user_in_db:
+            raise ValueError('Użytkownik już istnieje.') 
+        user_id = db.add_user(
+            self.first_name,
+            self.last_name,
+            self.email,
+            self.phone_number,
+            self.user_name,
+            self.password,
+            self.user_type_id
+            )
+        return user_id
+    
+    def save_guest_info(self):
+        if db.get_user('email', self.email):
+            raise ValueError('Użytkownik już istnieje.')
+        user_id = db.add_user(
+            self.first_name,
+            self.last_name,
+            self.email,
+            self.phone_number,
+            None,
+            None,
+            self.user_type_id
+            )
+        return user_id
+
+    @classmethod
+    def login(cls, username, password):
+        user = db.get_user('userName', username)
+        if not user:
+            return None
+        elif not check_password_hash(user['password'], password):
+            return None
+        else:
+            return cls(
+                    id = user['id'],
+                    first_name = user['firstName'],
+                    last_name = user['lastName'],
+                    email = user['email'],
+                    phone_number = user['phoneNumber'],
+                    user_name = user['userName'],
+                    password = user['password'],
+                    user_type_id = user['userTypeId']
+                )
+
+    # Kelner=3, Menadżer=4, Właściciel=5
+    @property
+    def is_staff(self):
+        return self.user_type_id in [3, 4, 5]
+
+    @property
+    def is_manager(self):
+        return self.user_type_id in [4, 5]
+    
+    #display user by username returns selected user
+    def display_user(self):
+        selected_user = db.get_user('userName', self.user_name)
+        if selected_user:
+            return selected_user
+        else: 
+            raise ValueError ('Użytkownik nie istnieje.')
+        
+    def get_user_by_email(self): 
+        selected_user = db.get_user('email', self.email)
+        if selected_user:
+            return True
+        else: 
+            return False
+    #reset password
+    #modify user
+    #delete user
+
+class Reservation:
+    def __init__(self, date, start_time, end_time, number_of_people, user_id=None, reservation_id=None):
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
+        self.number_of_people = number_of_people
+        self.user_id = user_id
+        self.id = reservation_id
+    
+    def check_available_tables(self) -> list[dict]:
+        available_tables = db.check_for_available_tables(self.date, self.start_time, self.end_time, self.number_of_people)
+        return available_tables
+
+    #add reservation
+    def add_reservation(self, selected_table: int) -> int:
+        new_reservation_id = db.create_reservation(
+            selected_table,
+            self.date,
+            self.start_time,
+            self.end_time,
+            self.number_of_people,
+            self.user_id
+        )
+        return new_reservation_id
+    
+    #modify reservation
+    def modify_reservation(self, attributes_to_change):
+        # 1. Aktualizacja obiektu w Pythonie
+        for key, value in attributes_to_change.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        if self.id is None:
+            raise ValueError("Nie można modyfikować rezerwacji, która nie ma ID!")
+        else: 
+            return db.modify_reservation(
+                self.date, self.start_time, self.end_time, self.number_of_people, 
+                self.id
+            )
+#display user by role, returns a list of all users with that role
+def display_users_by_role(column):
+    users_with_selected_role = db.get_users_by_role(user_type_id= int)
+    if users_with_selected_role:
+        return users_with_selected_role
+    else:
+        raise ValueError ('Nie ma użytkowników z wybraną rolą.')
